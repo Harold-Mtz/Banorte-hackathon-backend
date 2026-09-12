26b9af25199071b20d7c5b54394340bd9438f67f
Harold Isaac Hernandez Martinez <martinezharold059@gmail.com>
2026-09-12 05:02:12 -0600

    se agregan funcionalidades para las goals

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,23 @@
 import express from 'express';
 import lifeEventRoutes from './routes/life-event.routes';
+import savingsGoalRoutes from './routes/savings-goal.routes';
+import cors from 'cors';
+
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 
 app.use(
   '/api/life-events',
   lifeEventRoutes
+);
+
+app.use(
+  '/api/savings-goals',
+  savingsGoalRoutes
 );
 
 export default app;
